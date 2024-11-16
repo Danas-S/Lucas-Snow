14bc54322864e78c6a4eeba8898d8df994ac93e5
Ruslan Rohovskyi - C24741821 <c24741821@mytudublin.ie>
2024-11-16 13:46:53 +0000

    Mountain jew update (Trips page)

--- js/scripts.js
+++ js/scripts.js
@@ -7,34 +7,61 @@ document.addEventListener('DOMContentLoaded', () => {
     const headerMenu = document.querySelector('header');
     const mobileMenu = document.querySelector('.mobile-menu');
     const hoverPopup = document.querySelector('.hover-popup');
+    const discountOpen = document.querySelector('.discount-button');
+    const mountainJew = document.querySelector('.mountain-jew');
+    const mountainJewAudio = mountainJew.querySelector('audio');
+    
+    //Modifiers
+    const show = 'show';
+    const popupOpened = 'popup-opened';
     
     // Event Listeners
     searchPanelHeader.addEventListener('click', e => {
-        searchPopup.classList.add('show');
-        document.body.classList.add('popup-opened');
+        searchPopup.classList.add(show);
+        document.body.classList.add(popupOpened);
     });
     searchPanelFooter.addEventListener('click', e => {
-        searchPopup.classList.add('show');
-        document.body.classList.add('popup-opened');
+        searchPopup.classList.add(show);
+        document.body.classList.add(popupOpened);
     });
     searchPopupClose.addEventListener('click', e => {
-        searchPopup.classList.remove('show');
-        document.body.classList.remove('popup-opened');
+        searchPopup.classList.remove(show);
+        document.body.classList.remove(popupOpened);
     });
     mobileMenu.addEventListener('click', e => {
-        if(headerMenu.classList.contains('show')) {
-            headerMenu.classList.remove('show');
-            hoverPopup.classList.remove('show');
-            document.body.classList.remove('popup-opened');
+        if(headerMenu.classList.contains(show)) {
+            headerMenu.classList.remove(show);
+            hoverPopup.classList.remove(show);
+            document.body.classList.remove(popupOpened);
         } else {
-            headerMenu.classList.add('show');
-            hoverPopup.classList.add('show');
-            document.body.classList.add('popup-opened');
+            headerMenu.classList.add(show);
+            hoverPopup.classList.add(show);
+            document.body.classList.add(popupOpened);
         }
     });
     hoverPopup.addEventListener('click', e => {
-        headerMenu.classList.remove('show');
-        hoverPopup.classList.remove('show');
-        document.body.classList.remove('popup-opened');
+        if(headerMenu.classList.contains(show)) {
+            headerMenu.classList.remove(show);
+        }
+        if(mountainJew.classList.contains(show)) {
+            mountainJew.classList.remove(show);
+            mountainJew.querySelector('audio').pause();
+        }
+        hoverPopup.classList.remove(show);
+        document.body.classList.remove(popupOpened);
+    });
+    discountOpen.addEventListener('click', e => {
+        document.body.classList.add(popupOpened);
+        e.target.remove();
+        hoverPopup.classList.add(show);
+        mountainJew.classList.add(show);
+        mountainJewAudio.currentTime = 0;
+        mountainJewAudio.play();
+    });
+    mountainJew.addEventListener('click', e => {
+        document.body.classList.remove(popupOpened);
+        hoverPopup.classList.remove(show);
+        mountainJew.classList.remove(show);
+        mountainJew.querySelector('audio').pause();
     });
 });
